82a486251de5a5bfbc6d254a2eca0a82635376b1
asumihiroki <asumin-ap.29@ezweb.ne.jp>
2022-03-01 13:47:51 +0900

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,11 @@ import { Product } from '../shared/product.model';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  product:Product[] = products;
+  product:Product[] = {
+    name: "太郎",
+    price: 500,
+    description: "ああああ",
+  };
 
   share() {
     window.alert('The product has been shared!');
--- src/app/shared/product.model.ts
+++ src/app/shared/product.model.ts
@@ -1,5 +1,5 @@
 export class Product {
-  name: string;
-  price: number;
-  description: string;
+  name:string;
+  price:number;
+  description:string;
 }
